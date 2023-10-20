45d33d09e35c05b7c1e8195e0f620c3f0f823e31
TheKanu <keanu.amann@outlook.com>
2023-10-20 10:07:26 +0200

    Fixed deleted bug, should work as intended now

--- src/actions/dbActions.js
+++ src/actions/dbActions.js
@@ -1,74 +1,63 @@
 import { isCurrentDateTimeClose } from "../index.js";
 import { interactionMapper } from "../mapper/interactionMapper.js";
 
-export const getAppointments = async (db) => {
-  try {
-    return await new Promise((resolve, reject) => {
-      db.all(`SELECT * FROM ${process.env.tableName}`, [], (err, rows) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(rows);
-        }
-      });
+export const getAppointments = (db) => {
+  return new Promise((resolve, reject) => {
+    db.all(`SELECT * FROM ${process.env.tableName}`, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
     });
-  } catch (err_1) {
-    console.error(err_1);
-  }
+  });
 };
 
 export const deleteAppointment = (db, data) => {
   const appointment = interactionMapper(data);
 
   const query = `
-  DELETE FROM ${process.env.tableName}
-  WHERE appointment = ? AND date = ? AND channelId = ?
-`;
-  try {
-    db.all(
-      query,
-      [appointment.appointment, appointment.date, appointment.channelId],
-      (err) => {
-        if (err) {
-          console.error("Error deleting record:", err.message);
-        } else {
-          console.log("Record deleted successfully." + appointment.appointment);
-        }
-      }
-    );
-  } catch (err_1) {
-    console.error(err_1);
-  }
+    DELETE FROM ${process.env.tableName}
+    WHERE appointment = ? AND date = ? AND channelId = ?
+  `;
+
+  db.run(query, [appointment.appointment, appointment.date, appointment.channelId], (err) => {
+    if (err) {
+      console.error("Error deleting record:", err.message);
+      throw err; // Throw the error to be handled externally
+    } else {
+      console.log("Record deleted successfully: " + appointment.appointment);
+    }
+  });
 };
 
-export const addAppointment = async (db, interaction) => {
+export const addAppointment = (db, interaction) => {
   const mappedInteraction = interactionMapper(interaction);
-  const dateTimeRegex =
-    /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4} (\d{2}:\d{2})$/;
-  const DEdateTimeRegex =
-    /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.\d{4} (\d{2}:\d{2})$/;
+  const dateTimeRegex = /^\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}$/;
 
-  const stmt = db.prepare(
-    `INSERT INTO ${process.env.tableName} VALUES (?, ?, ?, ?)`
-  );
+  const stmt = db.prepare(`INSERT INTO ${process.env.tableName} VALUES (?, ?, ?, ?)`);
 
   if (isCurrentDateTimeClose(mappedInteraction.date) < 0) {
-    return interaction.reply(process.env.AppointmentInThePastMsg);
+    throw new Error(process.env.AppointmentInThePastMsg);
   }
 
-  if (
-    dateTimeRegex.test(mappedInteraction.date) ||
-    DEdateTimeRegex.test(mappedInteraction.date)
-  ) {
+  if (dateTimeRegex.test(mappedInteraction.date)) {
     stmt.run(
       interaction.user.id,
       mappedInteraction.appointment,
       mappedInteraction.date,
-      mappedInteraction.channelId
+      mappedInteraction.channelId,
+      (err) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        } else {
+          console.log("New appointment added successfully");
+        }
+      }
     );
-    await stmt.finalize();
-    interaction.reply(process.env.newAppointmentMsg);
-    return;
+    stmt.finalize();
+  } else {
+    throw new Error("Failed to create, check your format");
   }
-  interaction.reply("Failed to create, check your format");
 };
